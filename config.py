621663ad7b01acc46bda6128dd5a0a2734607b44
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2025-04-10 09:54:30 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,27 @@
+# Pygame Configuration Module
+
+# Color constants (RGB)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+GRAY = (128, 128, 128)
+DARK_RED = (128, 0, 0)
+DARK_GREEN = (0, 128, 0)
+DARK_BLUE = (0, 0, 128)
+PURPLE = (160, 32, 240, 255)
+# Game window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Frame rate (frames per second)
+FPS = 60
+
+
+# title
+TITLE ='pygame template'
+
---
+++ draw.py
@@ -0,0 +1,47 @@
+
+#move stuff
+
+#def handle_events (x1,y1):
+   # for event in pygame.event.get():
+       # if event.type == pygame.QUIT:
+            #return x1, y1, False
+        
+   #keys = pygame.key.get_pressed()
+   #if keys[pygame.K_UP]and y1>0:
+   #    y1 -=10
+   # if keys[pygame.K_DOWN]and y1<600:
+   #    y1+=10
+   # if keys[pygame.K_LEFT]and x1>0:
+   #     x1-=10
+   # if keys[pygame.K_RIGHT] and x1<800:
+   #     x1+=10
+#return x1,y1, True
+
+#Animate
+
+    # x=400
+    # y=300
+    # new_y=5
+    # new_x=5
+
+    # while running
+    #     x += new_x
+    #     y += new_y
+    #     if x >750 or x <0:
+    #         new_x = new_x*-1
+    #     if y >540 or y<0:
+    #         new_y = new_y*-1
+
+
+
+#draw stuff
+
+
+#def draw_text(screen, text, font, font_color, position, anti_aliased=True, italic=False, bold=False):
+    #img = font.render(text, True, font_color)
+    #screen.blit(img, position,)
+
+
+# def draw_rect(screen,x,y,width,height):
+#     pygame.draw.rect(screen,config.PURPLE,(x,y, width, height))
+
